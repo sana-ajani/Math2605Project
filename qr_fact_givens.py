e4b16fabb438a5238b0df6275208e50327227975
Sana Ajani <sanaajani@msn.com>
2015-11-22 20:51:02 -0500

    QR decomp - givens

--- qr_fact_givens.py
+++ qr_fact_givens.py
@@ -11,63 +11,38 @@ def givens(R, a, b):
         cos = float(base) / math.sqrt(base**2 + second**2)
         sin = float(second) / math.sqrt(base**2 + second**2)
 
+    else:
+        cos = 1
+        sin = 0
+    return cos, sin
 
     identity = np.eye(A.shape[0])
 
 
-
-
-def qr_fact_givens(A, b):
-    copyA = np.copy(A)
+def qr_fact_givens(matrixA):
+    copyA = np.copy(matrixA)
+    m = copyA.shape[0]
+    n = copyA.shape[1]
+    Q = np.eye(m)
+    R = copyA
     gList = []
     G = 0
-    for y in range(copyA.shape[0]):
-        columnList = copyA[:,y].tolist()
-        counter = 0
-        for num in columnList[y+1:copyA.shape[1]]:
-            columnList = copyA[:,y].tolist()
-            #print copyA
-            base = columnList[y]
-            #print base
-            second = num
-            if (num = 0):
-                cos = 1
-                sin = 0
-            else:
-                r = math.sqrt(base**2 + second**2)
-                cos = float(base) / r
-                sin = -1 * float(num) / r
-
-                identity = eye(copyA.shape[0])
-                identity[y, y] = cos
-                identity[y + (counter+1), y] = sin
-                #print "num =", num
-                identity[y, y+(counter+1)] = -sin
-                identity[y+(counter+1), y+counter+1] = cos
-                counter += 1
+    for i in range(m):
+        for j in range(n):
+            if i is j:
+                pivot_xy = (j, i)
+            if i < j and R[j, i]:
+                identity = np.eye(m)
+                cos, sin = givens(R, pivot_xy, (j, i))
+                identity[j][j] = cos
+                identity[i][i] = cos
+                identity[i][j] = sin
+                identity[j][i] = -sin
                 G = identity
-                #print "G = ",G
                 gList.append(identity)
-                copyA = matrixMult(G, copyA)
-    R = copyA
-    Q = eye(A.shape[0])
+                R = np.dot(identity, R)
     for g in gList:
         gT = g.transpose()
-        Q = matrixMult(Q,gT)
+        Q = np.dot(Q,gT)
 
-    errorMatrix = matrixMult(Q,R) - A
-    #print "error matrix =", errorMatrix
-    maximum = 0
-    for y in range(errorMatrix.shape[0]):
-        for x in range(errorMatrix.shape[1]):
-            if abs(errorMatrix[x,y]) > maximum:
-                maximum = abs(errorMatrix[x,y])
-    #print "max =", maximum
-    x0 = solve_qr_b(Q, R, b)
-    hErrorMatrix = matrixMult(A,x0) - b
-    hxerror = 0
-    for y in range(hErrorMatrix.shape[0]):
-        for x in range(hErrorMatrix.shape[1]):
-            if abs(hErrorMatrix[y,x]) > hxerror:
-                hxerror = abs(hErrorMatrix[y,x])
-    return Q, R, maximum, x0, hxerror
+    return Q, R
